df1fbbea1260305cd17bb0a5a9fcda882bc0886b
Ondrej Sika <ondrej@ondrejsika.com>
2025-04-08 07:27:07 +0000

    feat: Add /metrics with custom metric example_requests

--- main.go
+++ main.go
@@ -3,12 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var counter_requests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "example",
+		Name:      "requests",
+		Help:      "Number of requests",
+	})
+
 func main() {
+	prometheus.MustRegister(counter_requests)
+
+	http.Handle("/metrics", promhttp.Handler())
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		counter_requests.Inc()
 		fmt.Fprintf(w, "Hello World!\n")
 	})
+
 	fmt.Println("Listen on 0.0.0.0:8000, see http://127.0.0.1:8000")
 	http.ListenAndServe(":8000", nil)
 }
