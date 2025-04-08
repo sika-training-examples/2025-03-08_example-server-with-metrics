ddb314545228fac5c12ec2900ead1c88ae02d76d
Ondrej Sika <ondrej@ondrejsika.com>
2025-04-08 08:10:56 +0000

    feat: Add example queue (metrics, endpoints)

--- main.go
+++ main.go
@@ -17,14 +17,52 @@ var counter_requests = prometheus.NewCounterVec(
 	[]string{"method", "path", "status_code"},
 )
 
+var queue = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Namespace: "example",
+		Name:      "queue",
+		Help:      "Nuber of events in queue",
+	},
+)
+
+var queueInt int
+
 func main() {
 	prometheus.MustRegister(counter_requests)
+	prometheus.MustRegister(queue)
 
 	http.Handle("/metrics", promhttp.Handler())
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		counter_requests.WithLabelValues(r.Method, r.URL.Path, "200").Inc()
 		fmt.Fprintf(w, "Hello World!\n")
+	})
+
+	http.HandleFunc("/inc", func(w http.ResponseWriter, r *http.Request) {
+		counter_requests.WithLabelValues(r.Method, r.URL.Path, "200").Inc()
+		queue.Inc()
+		queueInt++
+		fmt.Fprintf(w, "+1 (%d)\n", queueInt)
+	})
+
+	http.HandleFunc("/dec", func(w http.ResponseWriter, r *http.Request) {
+		if queueInt == 0 {
+			counter_requests.WithLabelValues(r.Method, r.URL.Path, "500").Inc()
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "error: queue is empty\n")
+			return
+		}
+
+		counter_requests.WithLabelValues(r.Method, r.URL.Path, "200").Inc()
+		queue.Dec()
+		queueInt--
+		fmt.Fprintf(w, "-1 (%d)\n", queueInt)
+	})
+
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		counter_requests.WithLabelValues(r.Method, r.URL.Path, "404").Inc()
+		w.WriteHeader(404)
+		fmt.Fprintf(w, "404 Not Found\n")
 	})
 
 	fmt.Println("Listen on 0.0.0.0:8000, see http://127.0.0.1:8000")
